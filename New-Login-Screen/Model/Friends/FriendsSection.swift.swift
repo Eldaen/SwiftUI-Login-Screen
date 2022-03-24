1af0dd0848acacef9928d5f9b474cf2597cdabce
Sizov Denis <sv_skatch@bk.ru>
2022-03-24 13:02:08 +0300

    Moved static data to the extension

--- New-Login-Screen/Model/Friends/FriendsSection.swift.swift
+++ New-Login-Screen/Model/Friends/FriendsSection.swift.swift
@@ -16,3 +16,39 @@ struct FriendsSection: Identifiable {
 	let name: String
 	let friends: [Friend]
 }
+
+extension FriendsSection {
+	static var data = [
+		FriendsSection(name: "V",
+					   friends: [
+						Friend(name: "Vasia", imageName: "", images: [
+							FriendImage(name: "person"),
+							FriendImage(name: "person"),
+							FriendImage(name: "person"),
+							FriendImage(name: "person"),
+						]),
+						Friend(name: "Vania", imageName: "", images: [])
+					   ]),
+		FriendsSection(name: "K",
+					   friends: [
+						Friend(name: "Katia", imageName: "", images: []),
+						Friend(name: "Kiril", imageName: "", images: [])
+					   ]),
+		FriendsSection(name: "P",
+					   friends: [
+						Friend(name: "Petia", imageName: "", images: [])
+					   ]),
+		FriendsSection(name: "S",
+					   friends: [
+						Friend(name: "Sasha", imageName: "", images: [])
+					   ]),
+		FriendsSection(name: "M",
+					   friends: [
+						Friend(name: "Masha", imageName: "", images: [])
+					   ]),
+		FriendsSection(name: "D",
+					   friends: [
+						Friend(name: "Dasha", imageName: "", images: [])
+					   ]),
+	]
+}
--- New-Login-Screen/View/Friends/FriendsView.swift
+++ New-Login-Screen/View/Friends/FriendsView.swift
@@ -11,44 +11,9 @@ import SwiftUI
 struct FriendsView: View {
 	
 	/// Статические друзья
-	@State private var friends: [FriendsSection] = [
-		FriendsSection(name: "V",
-					   friends: [
-						Friend(name: "Vasia", imageName: "", images: [
-							FriendImage(name: "person"),
-							FriendImage(name: "person"),
-							FriendImage(name: "person"),
-							FriendImage(name: "person"),
-						]),
-						Friend(name: "Vania", imageName: "", images: [])
-					   ]),
-		FriendsSection(name: "K",
-					   friends: [
-						Friend(name: "Katia", imageName: "", images: []),
-						Friend(name: "Kiril", imageName: "", images: [])
-					   ]),
-		FriendsSection(name: "P",
-					   friends: [
-						Friend(name: "Petia", imageName: "", images: [])
-					   ]),
-		FriendsSection(name: "S",
-					   friends: [
-						Friend(name: "Sasha", imageName: "", images: [])
-					   ]),
-		FriendsSection(name: "M",
-					   friends: [
-						Friend(name: "Masha", imageName: "", images: [])
-					   ]),
-		FriendsSection(name: "D",
-					   friends: [
-						Friend(name: "Dasha", imageName: "", images: [])
-					   ]),
-	]
+	@State private var friends: [FriendsSection] = FriendsSection.data
 	
     var body: some View {
-		
-		
-		
 		List {
 			ForEach(friends) { friendSection in
 				Section(header: Text(friendSection.name)) {
